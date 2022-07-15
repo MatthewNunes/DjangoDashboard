59a0e8934d38af0363435228e9d9996acc54cb0c
Matthew <matthewnunes@hotmail.com>
2022-07-15 16:34:16 +0100

    Can communicate between Django and JavaScript

--- IDS/Dashboard/views.py
+++ IDS/Dashboard/views.py
@@ -3,8 +3,15 @@ from django.http import HttpResponse
 from django.templatetags.static import static
 import pandas as pd
 import os
+import json
 
 # Create your views here.
 def index(request):
     network_df = pd.read_csv(static("Dashboard/resources/SWaT/data2017_time_SWaT.csv").replace("/", "./Dashboard/", 1))
-    return render(request, "Dashboard/index.html", {'dataset': network_df.to_json()})
+    dst_file = open(static("Dashboard/resources/SWaT/2017_SWAT_dst_ips.json").replace("/", "./Dashboard/", 1))
+    dst_obj = json.load(dst_file)
+    print(dst_obj)
+    return render(request, "Dashboard/index.html", {
+        'dataset': network_df[0:2].to_json(orient="records"),
+        'destination_ips': json.dumps(dst_obj)
+        })
