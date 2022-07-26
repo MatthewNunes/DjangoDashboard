c74e478b1367505e729bc97aa9485018dde340fd
Matthew <matthewnunes@hotmail.com>
2022-07-26 17:39:06 +0100

    Can move between time values on main index page

--- IDS/Dashboard/views.py
+++ IDS/Dashboard/views.py
@@ -7,13 +7,26 @@ import json
 from .models import Device
 
 
+network_df = pd.read_csv(static("Dashboard/resources/SWaT/data2017_time_SWaT.csv").replace("/", "./Dashboard/", 1))
+time_list = network_df["StartTime"].unique()
+current_time = time_list[0]
+
 def selectData():
     pass
 
 # Create your views here.
 def index(request):
-    network_df = pd.read_csv(static("Dashboard/resources/SWaT/data2017_time_SWaT.csv").replace("/", "./Dashboard/", 1))
-    network_slice_df = network_df[network_df["StartTime"] == '14-06-2017 03:50:00.000000']
+    global current_time
+    global time_list
+    global network_df
+
+    if request.method == 'GET':
+        if (request.GET.get('time') != None):
+            current_time = request.GET.get('time')
+            print(current_time)
+
+    network_slice_df = network_df[network_df["StartTime"] == current_time]
+    print(network_slice_df.shape)
 
     dst_file = open(static("Dashboard/resources/SWaT/2017_SWAT_dst_ips.json").replace("/", "./Dashboard/", 1))
     dst_obj = json.load(dst_file)
@@ -31,10 +44,12 @@ def index(request):
         'source_ips': json.dumps(src_obj),
         'unique_ips': json.dumps(unique_ips_obj),
         'source_model': Device.objects.all(),
+        'time_list': json.dumps(list(time_list)),
+        'current_time': current_time,
         })
 
 def table(request):
-    network_df = pd.read_csv(static("Dashboard/resources/SWaT/data2017_time_SWaT.csv").replace("/", "./Dashboard/", 1))
+    #network_df = pd.read_csv(static("Dashboard/resources/SWaT/data2017_time_SWaT.csv").replace("/", "./Dashboard/", 1))
     network_slice_df = network_df[network_df["StartTime"] == '14-06-2017 03:50:00.000000']
 
     src_file = open(static("Dashboard/resources/SWaT/2017_SWAT_src_ips.json").replace("/", "./Dashboard/", 1))
---
+++ readme.md
@@ -0,0 +1,2 @@
+# User-centered IDS built around the SWaT dataset
+This is a proof of concept IDS dashboard that is built using Django, D3, HTML & CSS.
